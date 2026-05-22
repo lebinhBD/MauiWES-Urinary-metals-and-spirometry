37065351b304fc3d7139972a5a8fd11c697a1a2a
lebinhBD <103030795+lebinhBD@users.noreply.github.com>
2026-05-22 09:59:05 -1000

    Add files via upload

---
+++ code/TableS4_HMDescriptive.R
@@ -0,0 +1,27 @@
+library(dplyr)
+library(gtsummary)
+
+
+HM_inMaui_clean_des <- readRDS("~/Documents/Work/PAPERS/MauiWES_heavymetal/heavymetal_spiro/heavymetals_lung_103025/HM_inMaui_clean_des.rds")
+
+data_metal <- HM_inMaui_clean_des
+
+
+## descriptive table for lung function by locations ## 
+
+table_gtsummary <- data_metal %>%
+  tbl_summary(
+    type = all_categorical() ~ "categorical",
+    by = location,
+    statistic = list(all_continuous() ~ "{median} (IQR: {p25}, {p75})",
+                     all_categorical() ~ "{n} ({p}%)"),
+    missing = "ifany",
+    missing_text = "(Missing: {n})",# Exclude missing data rows
+    digits = list(all_categorical() ~ c(0,2),
+                  digits = all_continuous() ~ 2)) %>%
+  add_p(test = list(
+    all_continuous() ~ "kruskal.test",
+    all_categorical() ~ "chisq.test"))%>%
+  as_gt() %>%
+  gt::gtsave(filename = "metal_summary_location.docx")
+
