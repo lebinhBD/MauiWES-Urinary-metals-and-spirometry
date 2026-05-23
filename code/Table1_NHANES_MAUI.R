db802874b0a2a2efc774ca3f5c1541105475e9e3
lebinhBD <103030795+lebinhBD@users.noreply.github.com>
2026-05-22 17:56:03 -1000

    Add files via upload

---
+++ code/Table1_NHANES_MAUI.R
@@ -0,0 +1,40 @@
+library(dplyr)
+library(tidyr)
+library(gtsummary)
+
+
+HM_nhanes_maui_notcorrected_111525 <- readRDS("~/Documents/Work/PAPERS/MauiWES_heavymetal/heavymetal_spiro/BKMR/NHANES_11_12/HM_nhanes_maui_notcorrected_111525.rds")
+
+
+data_sum <- HM_nhanes_maui_notcorrected_111525 %>% select(
+  barium,        
+  cadmium,
+  cobalt,        
+  cesium,
+  moly,         
+  magnanese,
+  lead,         
+  antimony,
+  tin,           
+  strontium,
+  thallium,      
+  tungsten,
+  uranium,
+  asenic,
+  maui    
+) 
+
+table_gtsummary <- data_sum %>%
+  tbl_summary(
+    by = maui,
+    statistic = list(all_continuous() ~ "{median} (IQR: {p25}, {p75})",
+                     all_categorical() ~ "{n} ({p}%)"),
+    missing = "ifany",
+    missing_text = "(Missing: {n})",# Exclude missing data rows
+    digits = list(all_categorical() ~ c(0,2),
+                  digits = all_continuous() ~ 2)) %>%
+  add_p(test = list(
+    all_continuous() ~ "t.test",
+    all_categorical() ~ "chisq.test"))%>%
+  as_gt() %>%
+  gt::gtsave(filename = "metals_nhanes_maui_111525.docx")
